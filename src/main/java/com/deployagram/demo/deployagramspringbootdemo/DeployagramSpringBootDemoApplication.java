a4d6c8eedbfad193fa547640f2ba1d36972a9937
Andy Muir <andy@deployagram.com>
2025-10-26 20:43:54 +0000

    Created new Spring Boot project

---
+++ src/main/java/com/deployagram/demo/deployagramspringbootdemo/DeployagramSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.deployagram.demo.deployagramspringbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployagramSpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployagramSpringBootDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deployagram/demo/deployagramspringbootdemo/DeployagramSpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deployagram.demo.deployagramspringbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployagramSpringBootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
